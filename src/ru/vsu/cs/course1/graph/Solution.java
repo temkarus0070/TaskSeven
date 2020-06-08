748a81bf4c4fcd74c431462f1e7f93e1029adc6b
temkarus0070 <pretkovartyom@gmail.com>
2020-06-08 19:43:37 +0300

    Initial commit

---
+++ src/ru/vsu/cs/course1/graph/Solution.java
@@ -0,0 +1,12 @@
+package ru.vsu.cs.course1.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static List<Integer> pathForBook(int bookOwner, Graph friendlyGraph){
+        List<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
